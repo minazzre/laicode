284782e2cbcf31207df02d71f37c7a3efaa10dd8
minazzre <20853243+minazzre@users.noreply.github.com>
2020-06-13 01:14:02 -0700

    Create SelectionSort.java

---
+++ src/sorting_algorithms/SelectionSort.java
@@ -0,0 +1,27 @@
+package sorting_algorithms;
+
+public class SelectionSort {
+    public int[] selectionSort(int[] array) {
+        if (array == null || array.length == 0) {
+            return array;
+        }
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int tmp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = tmp;
+            }
+        }
+        return array;
+    }
+}
+
+// time complexity: O(n)
+// space complexity: O(1)
+// stability: unstable
